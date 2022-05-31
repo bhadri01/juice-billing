4b1af85e2f3284ed667db46eb44436a04e482314
nocturnalplay <nocturnalplay0@gmail.com>
2022-06-01 01:22:28 +0530

    success

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    mongodburl: "mongodb://127.0.0.1:27017/userBDstore",
-  },
 };
 
 module.exports = nextConfig;
