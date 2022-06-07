ba00b5518ce7ee1d2c33300fc051ff78c4dbd7e6
bhadrinathan <59721580+bhadri2002@users.noreply.github.com>
2022-06-07 23:19:33 +0530

    Update finalprice.js
    address updated

--- pages/finalprice.js
+++ pages/finalprice.js
@@ -47,7 +47,7 @@ export default function Listadd() {
                 <span>invoice</span>
               </div>
               <div>
-                <span>Near old bus stand ,Colony road,Krishnagiri.635001</span>
+                <span>12/345 anna nagur, salem, tamilnadu, india-636006</span>
                 <span>{Date().toString().slice(0, 16)}</span>
               </div>
             </div>
@@ -81,7 +81,7 @@ export default function Listadd() {
             </table>
             <h2>thank&lsquo;s for shopping</h2>
             <div className="final-footer">
-              12/345 anna nagur, salem, tamilnadu, india-636006
+              Near old bus stand ,Colony road,Krishnagiri.635001
             </div>
           </div>
         </div>
